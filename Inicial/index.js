4f7e5af18adefdf561069cdd372aecdc5b28c672
Leonardo Romeiro <leozinwork@gmail.com>
2024-04-15 17:59:44 -0300

    Iniciando com JS

---
+++ Inicial/index.js
@@ -0,0 +1,11 @@
+console.log("Iniciando testes!");
+
+//Utilize sempre o Camel case no JS
+var nameDoAluno = "Leonardo Romeiro";
+var idade = 27, altura = 1.70
+var study = true
+var future;
+const gravidade=10
+let vento = "5km"
+//Tipagem fraca
+console.log(nameDoAluno+idade+altura, typeof estudando, typeof altura, typeof future);
---
+++ Inicial/mediaAluno.js
@@ -0,0 +1,6 @@
+const nota1 = 10
+const nota2 = 9
+
+const media = (nota1 + nota2) / 2;
+
+console.log("A media do aluno é "+ media)
